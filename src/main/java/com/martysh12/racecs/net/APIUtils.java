21f712081b08130900f04e6e0bac375d12cb9cc7
Martin K <martyshkon@gmail.com>
2023-08-27 05:25:36 +0300

    Remove unused import

--- src/main/java/com/martysh12/racecs/net/APIUtils.java
+++ src/main/java/com/martysh12/racecs/net/APIUtils.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Consumer;
 
 public class APIUtils {
     private static final String URL_BASE = "aircs.racing";
